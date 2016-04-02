5358977d8b9b866bf804b263d13ccc6d515b01f5
Erik Strom <strom2357@gmail.com>
2016-04-01 18:10:38 -0700

    Hello world

--- app/sw.js
+++ app/sw.js
@@ -1,24 +1,53 @@
-/*
-*
-*  Push Notifications codelab
-*  Copyright 2015 Google Inc. All rights reserved.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License
-*
-*/
+console.log('Started', self);
 
-// Version 0.1
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+    console.log('Installed', event);
+});
 
-'use strict';
+self.addEventListener('activate', function(event) {
+    console.log('Activated', event);
+});
 
-// TODO
+self.addEventListener('push', function(event) {
+    console.log('Push message received!', event);
+
+    var title = 'Push message!';
+
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: 'It\'s time to review your fading memories!',
+            icon: 'images/icon.png',
+            tag: 'my-tag'
+        })
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    console.log('Notification click: tag', event.notification.tag);
+    // Android doesn't close the notification when you click it
+    // See http://crbug.com/463146
+    event.notification.close();
+    var url = 'https://cerego.com/learn';
+    // Check if there's already a tab open with this URL.
+    // If yes: focus on the tab.
+    // If no: open a tab with the URL.
+    event.waitUntil(
+        clients.matchAll({
+            type: 'window'
+        })
+            .then(function(windowClients) {
+                console.log('WindowClients', windowClients);
+                for (var i = 0; i < windowClients.length; i++) {
+                    var client = windowClients[i];
+                    console.log('WindowClient', client);
+                    if (client.url === url && 'focus' in client) {
+                        return client.focus();
+                    }
+                }
+                if (clients.openWindow) {
+                    return clients.openWindow(url);
+                }
+            })
+    );
+});
